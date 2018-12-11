f0335ed5f69b7f2539d6bbb6254024a24f0f7307
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-11 17:32:48 +0100

    feat(Registration Flow): Functional Sign Up With Sendgrid confirmation mail :rocket:

--- Gemfile
+++ Gemfile
@@ -3,6 +3,16 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 
+gem 'devise'
+
+gem 'twitter-bootstrap-rails'
+
+gem 'bootstrap-sass', '~> 3.3'
+
+
+gem 'jquery-rails'
+gem 'sass-rails'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use postgresql as the database for Active Record
@@ -10,7 +20,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://tombrom-photo-app.herokuapp.com/', :protocol => 'https' }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+   config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -28,6 +31,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
